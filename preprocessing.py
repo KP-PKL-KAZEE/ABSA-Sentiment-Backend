2bd4a4d506b8420d9362588dc2d1a7ec254b0aba
dzakirafabillah <zigdzak27@gmail.com>
2022-09-23 09:45:11 +0700

    update preprocessing function

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,9 @@ import re
 import pandas as pd
 import time
 import numpy as np
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 
 # Function Clean Unused Sentence
 ## Function ini digunakan untuk menghilangkan sentences yang tidak memiliki aspect / emiten didalamnya.
@@ -19,36 +22,36 @@ def clean_sentences(sentences, aspect):
         new_sentences.append(sentence)
   return new_sentences
 
-# Function Remove Unused Dots
-## Function ini berfungsi menghilangkan titik (dots) yang tidak memiliki makna pada artikel.
-def remove_unused_dots(arr_of_char):
-  i=0
-  while i < len(arr_of_char):
-    if (arr_of_char[i] == "." and i != len(arr_of_char)):
-      if(arr_of_char[i - 1].isdigit()):
-        arr_of_char = arr_of_char[:i] + '' + arr_of_char[i+1:]
-    i = i+1
-  return "".join(arr_of_char)
+# Function Split Sentence
+## Function ini berfungsi untuk split artikel menjadi kumpulan kalimat bermakna
+def split_sentence(article):
+  tokens = nltk.tokenize.sent_tokenize(article)
+  return tokens
 
-# Function Preprocessing Text.
+# Function Preprocessing Text
 ## Function ini berfungsi menghilangkan kata yang tidak diperlukan pada artikel
-def preprocessing_text(sentences, aspect):
+def preprocessing_text(article, aspect):
   # hapus url
-  sentences = re.sub('\S+.com|\S+.co.id|\S+.co|\S+.id', '.', sentences)
+  article = re.sub('\S+.com|\S+.co.id|\S+.co|\S+.id', '.', article)
 
-  ### hapus titik yang bukan pemisah kalimat 
-  ### (yang pemisah angka, Tbk., PT.)
-  sentences = sentences.replace("PT.", "PT").replace("Tbk.", "Tbk")
-  sentences = remove_unused_dots(sentences)
+  ### hapus titik yang bukan pemisah kalimat(Tbk., PT.)
+  article = article.replace("PT.", "PT").replace("Tbk.", "Tbk")
 
-  ### Ambil kalimat yang mengandung emiten
-  sentences = sentences.split('.')
+  ### hapus titik yang bukan pemisah kalimat(titik dalam penomoran/urutan)
+  for number in range(100):
+    article = article.replace(" "+str(number)+". ", " " )
+
+  ### split menjadi kumpulan kalimat bermakna
+  sentences = split_sentence(article)
+
+  ### hanya ambil kalimat yang mengandung emiten
   arr_sentence_dirt = clean_sentences(sentences, aspect)
   
   arr_sentence_clean = []
   arr_sentence_clean.extend(arr_sentence_dirt)
   i = 0
   while (i < (len(arr_sentence_clean))):
+    # arr_sentence_dirt[i][0] = arr_sentence_dirt[i][0].upper()
     arr_sentence_clean[i] = re.sub(r'[^\w\d\s\.]+', '', arr_sentence_clean[i]) 
     arr_sentence_clean[i] = arr_sentence_clean[i].lower()
     ### Menghapus angka
